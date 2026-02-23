89ea2109b181804cf9312f2d1ff732bb3e631538
aacho04 <aaditinarayan14@gmail.com>
2026-02-23 19:32:03 +0530

    Navbar created

--- frontend/basic-react/src/components/Navbar.tsx
+++ frontend/basic-react/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
+import logo from "../assets/emergency.png";
+import './Navbar.css';
+
 function Navbar() {
   return (
-    <nav>
-      <h1>My Navbar</h1>
+    <nav className="navbar">
+    <div className="logo-container">
+    <img src={logo} alt="Emergency Coordination System Logo" className="logo" />
+  </div>
+     <ul className="nav-links">
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>Cover</li>
+        <li>Footer</li>
+      </ul>
     </nav>
   )
 }
